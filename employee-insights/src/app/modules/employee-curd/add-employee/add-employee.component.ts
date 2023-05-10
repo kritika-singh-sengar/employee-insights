d6e49916d81f9cba896c55f6da980b54e8afcb51
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-10 13:11:34 +0530

    created reactive form for add employee

--- employee-insights/src/app/modules/employee-curd/add-employee/add-employee.component.ts
+++ employee-insights/src/app/modules/employee-curd/add-employee/add-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
@@ -8,6 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class AddEmployeeComponent implements OnInit {
 
   constructor() { }
+
+  addEmployee = new FormGroup({
+    name: new FormControl(null, [Validators.pattern('[a-zA-Z ]*')]),
+    country: new FormControl(""),
+    skills: new FormControl(),
+    proficiency: new FormControl(),
+    mobile: new FormControl(),
+    email: new FormControl()
+  });
+
+  get name() { return this.addEmployee.get('name'); }
+  get country() { return this.addEmployee.get('country'); }
+  get skills() { return this.addEmployee.get('proficiency'); }
+  get mobile() { return this.addEmployee.get('mobile'); }
+  get email() { return this.addEmployee.get('email'); }
 
   ngOnInit(): void {
   }
