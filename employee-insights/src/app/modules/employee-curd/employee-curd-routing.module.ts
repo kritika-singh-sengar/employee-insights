5bb0d002bf4c99267bbf0dd86cb818be77632a37
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-10 12:55:59 +0530

    added routing for add and update

--- employee-insights/src/app/modules/employee-curd/employee-curd-routing.module.ts
+++ employee-insights/src/app/modules/employee-curd/employee-curd-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: 'update-employee', component: UpdateEmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
