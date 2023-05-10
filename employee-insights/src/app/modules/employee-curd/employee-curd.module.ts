e0373a4d9c4362c3571787647d3e6654408030ed
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-10 15:44:35 +0530

    imported httpClient module

--- employee-insights/src/app/modules/employee-curd/employee-curd.module.ts
+++ employee-insights/src/app/modules/employee-curd/employee-curd.module.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { EmployeeCurdRoutingModule } from './employee-curd-routing.module';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { UpdateEmployeeComponent } from './update-employee/update-employee.compo
   imports: [
     CommonModule,
     EmployeeCurdRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class EmployeeCurdModule { }
