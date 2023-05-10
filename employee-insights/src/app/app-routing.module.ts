508262b968f702b08745d2bf46365ea942692861
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-10 11:49:37 +0530

    lazy routes added for employee details and curd module

--- employee-insights/src/app/app-routing.module.ts
+++ employee-insights/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'employee-curd',
+    loadChildren: () => import('./modules/employee-curd/employee-curd.module').then(
+      (m) => m.EmployeeCurdModule
+    ),
+  },
+  {
+    path: 'employee-details',
+    loadChildren: () => import('./modules/employee-details/employee-details.module').then(
+      (m) => m.EmployeeDetailsModule
+    ),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
