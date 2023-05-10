16b1e6649b04c7e2ab241125aa90c2ffd2a3207b
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-10 11:54:48 +0530

    created update employee component

--- employee-insights/src/app/modules/employee-curd/employee-curd.module.ts
+++ employee-insights/src/app/modules/employee-curd/employee-curd.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { EmployeeCurdRoutingModule } from './employee-curd-routing.module';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
 
 
 @NgModule({
   declarations: [
-    AddEmployeeComponent
+    AddEmployeeComponent,
+    UpdateEmployeeComponent
   ],
   imports: [
     CommonModule,
