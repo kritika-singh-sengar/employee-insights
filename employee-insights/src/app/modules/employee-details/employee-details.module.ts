41b5dfb5d328e4b4a02260f8d3f9f04dc9a155d8
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-10 11:56:32 +0530

    created employee grid component

--- employee-insights/src/app/modules/employee-details/employee-details.module.ts
+++ employee-insights/src/app/modules/employee-details/employee-details.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { EmployeeDetailsRoutingModule } from './employee-details-routing.module';
+import { EmployeeGridComponent } from './employee-grid/employee-grid.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    EmployeeGridComponent
+  ],
   imports: [
     CommonModule,
     EmployeeDetailsRoutingModule
