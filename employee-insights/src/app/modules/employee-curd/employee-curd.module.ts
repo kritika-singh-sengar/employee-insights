778f635708cc24fa6ea31a93d0e9bfc73a6010aa
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-10 11:48:40 +0530

    created employee curd module

---
+++ employee-insights/src/app/modules/employee-curd/employee-curd.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeCurdRoutingModule } from './employee-curd-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmployeeCurdRoutingModule
+  ]
+})
+export class EmployeeCurdModule { }
