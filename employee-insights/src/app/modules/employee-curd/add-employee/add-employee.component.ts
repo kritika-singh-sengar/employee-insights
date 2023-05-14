5743516a2fdc26f5b793045b2a399b9d07b4086f
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-14 21:26:05 +0530

    updated for check box

--- employee-insights/src/app/modules/employee-curd/add-employee/add-employee.component.ts
+++ employee-insights/src/app/modules/employee-curd/add-employee/add-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { EmployeeCurdService } from '../employee-curd.service';
@@ -11,30 +11,74 @@ import { EmployeeCurdService } from '../employee-curd.service';
 })
 export class AddEmployeeComponent implements OnInit {
 
+  newEmployee!: FormGroup;
+  public selectedSkillsValue: string[] = [];
+  public employee: {
+    name: string, country: string, skills: string[],
+    proficiency: number, mobile: number, email: string
+  } = {
+      name: '',
+      country: '',
+      skills: [],
+      proficiency: 0,
+      mobile: 0,
+      email: ''
+    };
+  public skillsList = [
+    { id: 1, name: 'HTML', selected: false },
+    { id: 2, name: 'CSS', selected: false },
+    { id: 3, name: 'JS', selected: false }
+  ];
+
   constructor(private service: EmployeeCurdService,
     private toastr: ToastrService,
-    private router: Router) { }
+    private router: Router,
+    private fb: FormBuilder) {
 
-  newEmployee = new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
-    country: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z]*')]),
-    skills: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z]*')]),
-    proficiency: new FormControl(0, [Validators.required, Validators.pattern('[a-zA-Z]*')]),
-    mobile: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
-    email: new FormControl(null, [Validators.required, Validators.email])
-  });
+  }
+
+  addSkillsControls() {
+    const arr = this.skillsList.map(skill => {
+      return this.fb.control(skill.selected);
+    });
+    return this.fb.array(arr);
+  }
+
+  ngOnInit(): void {
+    this.newEmployee = this.fb.group({
+      name: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      country: new FormControl(null, [Validators.required, Validators.pattern('[a-zA-Z]*')]),
+      skills: this.addSkillsControls(),
+      proficiency: new FormControl(0, [Validators.required, Validators.pattern('[a-zA-Z]*')]),
+      mobile: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+      email: new FormControl(null, [Validators.required, Validators.email])
+    });
+  }
 
   get name() { return this.newEmployee.get('name'); }
   get country() { return this.newEmployee.get('country'); }
-  get skills() { return this.newEmployee.get('proficiency'); }
+  get skills() { return <FormArray>this.newEmployee.get('skills'); }
+  get proficiency() { return this.newEmployee.get('proficiency'); }
   get mobile() { return this.newEmployee.get('mobile'); }
   get email() { return this.newEmployee.get('email'); }
 
-  ngOnInit(): void {
+  getSelectedSkillsValue() {
+    this.selectedSkillsValue = [];
+    this.skills.controls.forEach((control, index) => {
+      if (control.value) {
+        this.selectedSkillsValue.push(this.skillsList[index].name);
+      }
+    });
   }
 
   onSubmit() {
-    this.service.addEmployee(this.newEmployee.value).subscribe((response) => {
+    this.employee.name = this.name?.value;
+    this.employee.country = this.country?.value;
+    this.employee.skills = this.selectedSkillsValue;
+    this.employee.proficiency = this.proficiency?.value;
+    this.employee.email = this.email?.value;
+    this.employee.mobile = this.mobile?.value;
+    this.service.addEmployee(this.employee).subscribe((response) => {
       this.newEmployee.reset();
       this.toastr.success("Employee Details Added Successfully!!", "Successful");
       this.toastr.info("Page Will Redirect Automatically!!", 'Info');
