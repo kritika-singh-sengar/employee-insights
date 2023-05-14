e99d82bdab1b3825884ebfdb0bdea2a97eebd841
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-14 21:20:21 +0530

    updated action cell render

--- employee-insights/src/app/shared/components/ag-grid-custom-cell-renders/action-custom-render.component.ts
+++ employee-insights/src/app/shared/components/ag-grid-custom-cell-renders/action-custom-render.component.ts
@@ -1,21 +1,55 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
-import { ICellRendererParams } from 'ag-grid-community';
+import { GridApi, ICellRendererParams } from 'ag-grid-community';
+import { EmployeeCurdService } from 'src/app/modules/employee-curd/employee-curd.service';
+import { EMPLOYEE } from '../../models/employee.model';
 
 @Component({
-    selector: 'action-custom-cell-render',
-    template: ``,
-    styles: [``],
+  selector: 'action-custom-cell-render',
+  template: `<span class="action-container cursor-pointer">
+      <span class="icon-pencil" title="edit">
+        <button type="button" (click)="editSelectedEmployeeDetails()">          
+          <i class="fa fa-pencil" aria-hidden="true"></i>
+        </button>
+      </span>
+      <span class="icon-delete" title="delete">
+        <button type="button" (click)="deleteSelectedEmployeeDetails()">
+          <i class="fa fa-trash" aria-hidden="true"></i>
+        </button>
+      </span>
+    </span>`,
+  styles: [`
+    .action-container{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    `],
 })
 export class ActionCustomCellRenderer implements ICellRendererAngularComp {
 
-    constructor() { }
-    agInit(params: ICellRendererParams<any, any, any>): void {
-        throw new Error('Method not implemented.');
-    }
-    refresh(params: ICellRendererParams<any, any, any>): boolean {
-        throw new Error('Method not implemented.');
-    }
+  public params!: ICellRendererParams;
 
+  constructor(private _curdService: EmployeeCurdService,
+    private router: Router,
+    private gridApi: GridApi) { }
 
+  agInit(params: ICellRendererParams): void {
+    this.params = params;
+  }
+
+  deleteSelectedEmployeeDetails(): void {
+    this.params.context.componentParent.empoloyeeId = this.params.data.id;
+    this.params.context.componentParent.deleteSelectedEmployeeDetails();
+  }
+
+  editSelectedEmployeeDetails(): void {
+    this.params.context.componentParent.empoloyeeId = this.params.data.id;
+    this.params.context.componentParent.editSelectedEmployeeDetails();
+  }
+
+  refresh(params: ICellRendererParams): boolean {
+    return false;
+  }
 }
