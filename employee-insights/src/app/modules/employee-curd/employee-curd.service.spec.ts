309896826159fa91aae2b05b2116570be7e7dfd8
kritikasinghsengar6421 <kritikasinghsengar6421@gmail.com>
2023-05-14 21:25:06 +0530

    configure for writing test

--- employee-insights/src/app/modules/employee-curd/employee-curd.service.spec.ts
+++ employee-insights/src/app/modules/employee-curd/employee-curd.service.spec.ts
@@ -1,16 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { EmployeeCurdService } from './employee-curd.service';
+import { of } from 'rxjs';
 
 describe('EmployeeCurdService', () => {
   let service: EmployeeCurdService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(EmployeeCurdService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return all employee details', () => {
+    //spyOn(service, 'getAllEmployee').and.returnValue(of({}));
+
   });
 });
